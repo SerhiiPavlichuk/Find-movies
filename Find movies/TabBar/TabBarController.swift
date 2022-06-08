0a0ef69723667bcb47484e888be96f78b0e6aabb
Serhii <S_Pavlichuk@icloud.com>
2022-06-08 15:11:25 +0300

    some refactoring

--- Find movies/TabBar/TabBarController.swift
+++ Find movies/TabBar/TabBarController.swift
@@ -21,14 +21,25 @@ class TabBarController: UITabBarController {
     }
 
     private func setupTabBar() {
-        let homeVC = UINavigationController(rootViewController: HomeScreenViewController())
-        homeVC.navigationBar.prefersLargeTitles = true
-        let bookmarkVC = UINavigationController(rootViewController: BookmarkScreenViewController())
-        let categoriesVC = UINavigationController(rootViewController: CategoriesScreenViewController())
+
         let bookmarkIcon: UIImage = .bookmarkIcon
         let userIcon: UIImage = .userIcon
         let homeIcon: UIImage = .homeIcon
         let images = [homeIcon, userIcon, bookmarkIcon]
+        customLayer()
+
+        self.setViewControllers(setupViewControllers(), animated: true)
+        self.tabBar.tintColor = .customWhite
+        self.tabBar.unselectedItemTintColor = .customGray
+        self.tabBar.backgroundColor = .clear
+        guard let items = self.tabBar.items else { return }
+
+        for x in 0..<images.count {
+            items[x].image = images[x]
+        }
+    }
+
+    private func customLayer() {
         let layer = CAShapeLayer()
         layer.path = UIBezierPath(roundedRect: CGRect(x: 60, y: self.tabBar.bounds.minY + 5, width: self.tabBar.bounds.width - 120, height: self.tabBar.bounds.height + 10), cornerRadius: (self.tabBar.frame.width/2)).cgPath
         layer.shadowColor = UIColor.lightGray.cgColor
@@ -47,14 +58,14 @@ class TabBarController: UITabBarController {
 
         tabBar.itemWidth = 50.0
         tabBar.itemPositioning = .centered
-        self.setViewControllers([homeVC, categoriesVC, bookmarkVC], animated: true)
-        self.tabBar.tintColor = .customWhite
-        self.tabBar.unselectedItemTintColor = .customGray
-        self.tabBar.backgroundColor = .clear
-        guard let items = self.tabBar.items else { return }
+    }
 
-        for x in 0..<images.count {
-            items[x].image = images[x]
-        }
+    private func setupViewControllers() -> [UIViewController]{
+        let homeVC = UINavigationController(rootViewController: HomeScreenViewController())
+        homeVC.navigationBar.prefersLargeTitles = true
+        let bookmarkVC = UINavigationController(rootViewController: BookmarkScreenViewController())
+        let categoriesVC = UINavigationController(rootViewController: CategoriesScreenViewController())
+        let controllers = [homeVC, bookmarkVC, categoriesVC]
+        return controllers
     }
 }
