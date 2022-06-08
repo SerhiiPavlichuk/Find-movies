8ce098e63dc2ffaa6dfd666124646a09d7cd5b38
Serhii <S_Pavlichuk@icloud.com>
2022-06-08 09:46:39 +0300

    Initial Commit

---
+++ Find movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find movies
+//
+//  Created by admin on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
